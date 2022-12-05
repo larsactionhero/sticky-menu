4deef20cd9b2560988c974e72e32b8eaf6870d98
Lars Ortlepp <job@larsactionhero.com>
2022-12-05 16:06:42 +0100

    changes in sticky mode

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  * @param {String} element element as string or already existing HTMLElement
  * @param {Object} options the options to pass. See constructor() fn for detailed list of options
  */
-export default class StickyMenu {
+ export default class StickyMenu {
   constructor(element, options) {
     this.startStickyAtPos = options.startStickyAtPos || 0;
     this.breakpoint = options.breakpoint || 0; // if breakpoint is set, init only above breakpoint
@@ -17,6 +17,7 @@ export default class StickyMenu {
     this.contentElementOffset = options.contentElementOffset;
     this.setContentOffset = options.setContentOffset || false;
     this.zIndex = options.zIndex || null;
+    this.isActive = false;
   }
 
   /**
@@ -31,6 +32,7 @@ export default class StickyMenu {
     let scrollTimer = null;
     let resizeTimer = null;
     window.addEventListener('scroll', () => {
+      this.setIsActive();
       if (scrollTimer) clearTimeout(scrollTimer);
       scrollTimer = setTimeout(() => {
         this.navPosition();
@@ -39,11 +41,16 @@ export default class StickyMenu {
 
     // call navPosition() if screen resizes (e.g. portrait->landscape)
     window.addEventListener('resize', () => {
+      this.setIsActive();
       if (resizeTimer) clearTimeout(resizeTimer);
       resizeTimer = setTimeout(() => {
         this.navPosition();
       }, 10);
     });
+  }
+
+  setIsActive() {
+    this.isActive = (window.scrollY > this.startStickyAtPos);
   }
 
   /**
@@ -56,25 +63,17 @@ export default class StickyMenu {
     if (window.innerWidth > this.breakpoint) {
       this.elementInitialOffsetTop = this.navElement.offsetTop;
 
-      // use value from options or nav height
-      if (!this.contentElementOffset) {
-        contentElementOffset = this.navElement.offsetHeight;
-      } else {
-        contentElementOffset = this.contentElementOffset;
-      }
+      contentElementOffset = (this.isActive) ? this.contentElementOffset : 0;
 
       // apply stickymenu mode:
-      if (window.scrollY > this.startStickyAtPos) {
-        contentElementOffset = this.contentElementOffset;
+      if (this.isActive) {
         this.activate();
       } else {
-        // remove stickymenu mode
-        contentElementOffset = 0;
         this.deactivate();
       }
 
       // apply offset to content element
-      if (this.contentElement !== null && this.setContentOffset === true) {
+      if (this.contentElement !== null && this.setContentOffset) {
         this.contentElement.style.marginTop = `${contentElementOffset}px`;
       }
     }
